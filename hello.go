8109acd6fa67871b8f7b3ef5f63297d506e92b5d
cheng <qianchengxx99@gmail.com>
2023-09-28 13:19:05 +0800

    updated test cases and refractored code

--- hello.go
+++ hello.go
@@ -2,17 +2,43 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const(
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+)
 
-func Hello(name string) string {
-	if name == ""{
+
+func Hello(name string, language string) string {
+	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
 
-func Bye() string{
+func greetingPrefix(language string) ( string){
+	prefix := englishHelloPrefix
+
+	switch language{
+	case "French":
+		prefix = frenchHelloPrefix
+	
+	case "Spanish":
+		prefix = spanishHelloPrefix
+
+	}
+
+	return prefix
+}
+
+
+
+
+
+
+
+func Bye() string {
 	return "Bye, world"
 }
 
--- hello_test.go
+++ hello_test.go
@@ -2,29 +2,47 @@ package main
 
 import "testing"
 
+
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris","")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("","")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T){
+		got := Hello("Benzema", "French")
+		want := "Bonjour, Benzema"
+		assertCorrectMessage(t,got,want)
+
 	})
 }
+
 
 func TestBye(t *testing.T) {
 	got := Bye()
 	want := "Bye, world"
 
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+
+func assertCorrectMessage(t testing.TB, got , want string){
+	t.Helper() // Hey, testing system, if something goes wrong in this helper function, show it as if it happened in the line where I called the helper." This makes it easier to spot and fix issues in your test code because the error message will point to the exact place in your test where the problem occurred.
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
