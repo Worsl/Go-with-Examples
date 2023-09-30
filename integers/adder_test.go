b6a4642eb2bdc94b133db76fec296ee91f03c406
cheng <qianchengxx99@gmail.com>
2023-09-30 17:05:25 +0800

    updated with all possible testing formats

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,7 +1,34 @@
 package integers
 import "testing"
 
-func TestAdder(t *testing.T){
+
+func TestAdder_Simple(t *testing.T){
+	sum := Add(2,2)
+	expected := 4
+
+	if sum != expected{
+		t.Errorf("Expected '%q' but got '%q' ",expected,sum)
+	}
+}
+
+func TestAdder_withCases_WithHelperFunctions(t *testing.T){
+	t.Run("Negative_Nums", func(t *testing.T) {
+		sum := Add(-500,-100)
+		expected := -600
+
+		assertCorrectMessage(t,expected,sum)
+	})
+
+	t.Run("mixed numbers",func(t *testing.T) {
+		sum := Add(-10,90)
+		expected:=80
+
+		assertCorrectMessage(t,expected,sum)
+
+	})
+}
+
+func TestAdder_withTableTesting(t *testing.T){
 
 	addTests := []struct{
 		num_array []int 
@@ -9,7 +36,7 @@ func TestAdder(t *testing.T){
 	}{
 		{ []int{2,2} ,4},
 		{ []int{100,2},102},
-		{ []int{-100,-900},-100},
+		{ []int{-100,-900},-1000},
 	}
 
 	for _, tt := range addTests{
@@ -21,4 +48,13 @@ func TestAdder(t *testing.T){
 		}
 	}
 
+}
+
+
+func assertCorrectMessage(t testing.TB, want int, got int ) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Expected '%q' but got '%q' ",want,got)
+		}
+	
 }
