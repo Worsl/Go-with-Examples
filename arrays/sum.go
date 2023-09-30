c321bb358c280f6abd1e49dae4779ed5ed9c771c
cheng <qianchengxx99@gmail.com>
2023-09-30 15:23:37 +0800

    completed slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,36 @@
 package array
 
-func Sum(numbers []int )( res int ){
 
-	for _,number := range numbers{
+
+func Sum(numbers []int) (res int) {
+
+	for _, number := range numbers {
 		res += number
 	}
-	return 
+	return
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0{
+			sums = append(sums,0)
+		}else{
+			tail := numbers[1:]
+			sums = append(sums,Sum((tail)))
+		}
+
+	}
+
+	return sums
 }
