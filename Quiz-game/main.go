354daecc337d2a502bbc1d7bb38ffcb5293b78e0
Worsl <qianchengxx99@gmail.com>
2023-10-12 12:30:27 +0800

    finished go QUiz

--- Quiz-game/main.go
+++ Quiz-game/main.go
@@ -1,49 +1,72 @@
 package main
 
-import(
-	"fmt"
+import (
 	"encoding/csv"
+	"flag"
+	"fmt"
 	"os"
+	"time"
 )
 
 
-func runGame( records [][]string) ( int) {
+func runGame( records [][]string, duration int) ( int) {
 	var userGuess string
 	var answer string
 	var score int
 
+	timer1 := time.NewTimer(time.Duration(duration) * time.Second)
+	answer_channel := make(chan string)
+
+
 	for _, record := range records {
+
 		answer = record[1]
 		fmt.Printf("%s =  ",record[0])
-		fmt.Scanln(&userGuess)
 
-		if userGuess == answer{
-			score += 1
-		}
+		// using a Goroutine to handle user input, which is a good approach to avoid blocking the program while waiting for user responses.
+		go func(){
+			fmt.Scanln(&userGuess)
+			answer_channel <- userGuess
+		}()
+
+		select {
+	
+		case recievedAnswer := <-answer_channel:
+			if recievedAnswer == answer{
+				score+=1
+			}
+
+		case <-timer1.C:
+			fmt.Println("Out of time!")
+			
+			return score
+
+		}	
 	}
 	return score
-
 } 
 
 func main(){
-	var duration int
-	fmt.Print("Timer duration: ")
-	fmt.Scanln(&duration)
 
-	file, err := os.Open("problems.csv")
+	limit := flag.Int("limit", 100000, "Sets a time limit for the program to run")
+	csv_file := flag.String("csv","problems.csv","a csv file in the format of 'question,answer")
+	flag.Parse()
+
+
+	file, err := os.Open(*csv_file)
 	if err != nil{
-		fmt.Println("error")
+		fmt.Printf("error, %s is not found in directory \n",*csv_file)
 	}
 
 	fileReader := csv.NewReader(file)
 	records,err:= fileReader.ReadAll()
 
 	if err != nil {
-        fmt.Println("Error:", err)
+        fmt.Println("Error no file records:", err)
         return
     }
 
-	score := runGame(records)
+	score := runGame(records, *limit)
 	fmt.Printf("Your score is %d \n", score)
 
 }
