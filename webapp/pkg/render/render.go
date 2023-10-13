0d060310753771778097cb8ade4f98cd6c503384
Worsl <qianchengxx99@gmail.com>
2023-10-13 17:05:13 +0800

    added 2nd method for caching/rendeirng

--- webapp/pkg/render/render.go
+++ webapp/pkg/render/render.go
@@ -3,33 +3,94 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
-func RenderTemplates(w http.ResponseWriter, tmpl string) {
-	// the first parameter takes in the extension of the base, while the 2nd paramter takes in the base itself
-	// this may not be optimal because render and reading from disks takes up a lot of space, why not use a cache?
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
+
+var pagemap = make(map[string]*template.Template)
+
+func RenderTemplatesOld(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, exists := pagemap[tmpl]
+
+	if !exists {
+		fmt.Printf("page {%s} not found in cache, parsing it and adding to cache\n",tmpl)
+		parsedTemplate, _ = template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html") // what if multiple bases?
+		pagemap[tmpl] = parsedTemplate
+
+	}
+
+
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error found", err)
 	}
+
 }
 
-var pagemap = make(map[string]*template.Template)
 
-func RenderTemplatesNew(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, exists := pagemap[tmpl]
+// RenderTemplatesNew have an advantage in that we can have multiple base layouts and it will be handled accordingly, with ParseGlob later
+func RenderTemplates(w http.ResponseWriter, tmpl string){
 
-	if !exists {
-		parsedTemplate, _ = template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
-		pagemap[tmpl] = parsedTemplate
-
-	} else {
-		err := parsedTemplate.Execute(w, nil)
-		if err != nil {
-			fmt.Println("error found", err)
-		}
+	// create a template cache (only if it first request?)
+	pageCache, err := createTemplateCache()
+	if err!= nil{
+		log.Fatal(err)
 	}
 
+	// get request template from cache
+	t,exists := pageCache[tmpl]
+	if !exists{
+		log.Fatal(err)
+	}
+
+	// render the template
+	err = t.Execute(w,nil)
+
+	if err!= nil{
+		fmt.Println("error parsing template:", err)
+	}
+
+
+}
+
+
+func createTemplateCache() (map[string]*template.Template,error){
+
+	myCache := map[string]*template.Template{}
+
+	// get all of the files named *.page.html first from ./templates
+	pages,err := filepath.Glob("./templates/*.page.html")
+
+	if err!= nil{
+		return myCache,err
+	}
+
+	// range through all the files ending with *.page.html
+	for _,page:= range(pages){
+		name := filepath.Base(page) // returns the last element of the path(filename) instead of entire path
+		ts,err := template.New(name).ParseFiles(page) // recall that page is the directory
+ 
+		if err!= nil{
+			return myCache,err
+		}
+
+		matches,err := filepath.Glob("./templates/*.layout.html")
+		if err!=nil{
+			return myCache,err
+		}
+
+		if len(matches) > 0{
+			ts,err = ts.ParseGlob("./templates/*.layout.html")
+			if err!=nil{
+				return myCache,err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache,nil
+
+	
 }
